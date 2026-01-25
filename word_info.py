2b560aa3e0962812b450af711a094ee0ce3e187c
Koen Middelhof <koen.middelhof@ennatuurlijk.nl>
2026-01-25 17:48:40 +0100

    idk

--- word_info.py
+++ word_info.py
@@ -1,15 +1,9 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
+from langchain import OpenAI
 
-# OpenAI model instellen
-chat_model = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.7)
-
-# Conversatieketen met geheugen
-conversation = ConversationChain(llm=chat_model, verbose=True)
-
-# Voorbeeldgesprek
-response = conversation.run("Hallo, kun je me iets vertellen over Ai-migo?")
+llm = OpenAI(temperature=0)
+response = llm("Hallo wereld")
 print(response)
+
 
 
 
